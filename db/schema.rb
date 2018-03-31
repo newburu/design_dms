da8c9e9bf63bb70ea662ea3760be5de9e65eddd5
Newburu <newburu@gmail.com>
2018-03-31 17:17:52 +0900

    DBをMySQLに変更

--- db/schema.rb
+++ db/schema.rb
@@ -12,29 +12,29 @@
 
 ActiveRecord::Schema.define(version: 20180323124804) do
 
-  create_table "collar_types", force: :cascade do |t|
+  create_table "collar_types", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4" do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "cuff_types", force: :cascade do |t|
+  create_table "cuff_types", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4" do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "designs", force: :cascade do |t|
+  create_table "designs", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4" do |t|
     t.string "name", null: false
-    t.integer "genre_id"
-    t.integer "collar_type_id"
-    t.integer "sleeve_type_id"
-    t.integer "cuff_type_id"
-    t.integer "swimsuit_type_id"
-    t.integer "tops_type_id"
-    t.integer "skirt_type_id"
-    t.integer "size_id"
-    t.integer "sex_id"
+    t.bigint "genre_id"
+    t.bigint "collar_type_id"
+    t.bigint "sleeve_type_id"
+    t.bigint "cuff_type_id"
+    t.bigint "swimsuit_type_id"
+    t.bigint "tops_type_id"
+    t.bigint "skirt_type_id"
+    t.bigint "size_id"
+    t.bigint "sex_id"
     t.text "memo"
     t.string "design_img"
     t.string "pattern_img"
@@ -43,7 +43,7 @@ ActiveRecord::Schema.define(version: 20180323124804) do
     t.integer "download_point", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "user_id"
+    t.bigint "user_id"
     t.index ["collar_type_id"], name: "index_designs_on_collar_type_id"
     t.index ["cuff_type_id"], name: "index_designs_on_cuff_type_id"
     t.index ["genre_id"], name: "index_designs_on_genre_id"
@@ -56,25 +56,25 @@ ActiveRecord::Schema.define(version: 20180323124804) do
     t.index ["user_id"], name: "index_designs_on_user_id"
   end
 
-  create_table "friendly_id_slugs", force: :cascade do |t|
+  create_table "friendly_id_slugs", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4" do |t|
     t.string "slug", null: false
     t.integer "sluggable_id", null: false
     t.string "sluggable_type", limit: 50
     t.string "scope"
     t.datetime "created_at"
-    t.index ["slug", "sluggable_type", "scope"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type_and_scope", unique: true
-    t.index ["slug", "sluggable_type"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type"
+    t.index ["slug", "sluggable_type", "scope"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type_and_scope", unique: true, length: { slug: 70, scope: 70 }
+    t.index ["slug", "sluggable_type"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type", length: { slug: 140 }
     t.index ["sluggable_id"], name: "index_friendly_id_slugs_on_sluggable_id"
     t.index ["sluggable_type"], name: "index_friendly_id_slugs_on_sluggable_type"
   end
 
-  create_table "genres", force: :cascade do |t|
+  create_table "genres", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4" do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "sessions", force: :cascade do |t|
+  create_table "sessions", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4" do |t|
     t.string "session_id", null: false
     t.text "data"
     t.datetime "created_at", null: false
@@ -83,49 +83,49 @@ ActiveRecord::Schema.define(version: 20180323124804) do
     t.index ["updated_at"], name: "index_sessions_on_updated_at"
   end
 
-  create_table "sexes", force: :cascade do |t|
+  create_table "sexes", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4" do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "sizes", force: :cascade do |t|
+  create_table "sizes", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4" do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "skirt_types", force: :cascade do |t|
+  create_table "skirt_types", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4" do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "sleeve_types", force: :cascade do |t|
+  create_table "sleeve_types", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4" do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "swimsuit_types", force: :cascade do |t|
+  create_table "swimsuit_types", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4" do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "tops_types", force: :cascade do |t|
+  create_table "tops_types", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4" do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "users", force: :cascade do |t|
+  create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4" do |t|
     t.integer "sign_in_count", default: 0
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string "current_sign_in_ip"
     t.string "last_sign_in_ip"
-    t.integer "uid", limit: 8
+    t.bigint "uid"
     t.string "name"
     t.string "screen_name"
     t.string "provider"
@@ -137,4 +137,14 @@ ActiveRecord::Schema.define(version: 20180323124804) do
     t.index ["uid"], name: "index_users_on_uid", unique: true
   end
 
+  add_foreign_key "designs", "collar_types"
+  add_foreign_key "designs", "cuff_types"
+  add_foreign_key "designs", "genres"
+  add_foreign_key "designs", "sexes"
+  add_foreign_key "designs", "sizes"
+  add_foreign_key "designs", "skirt_types"
+  add_foreign_key "designs", "sleeve_types"
+  add_foreign_key "designs", "swimsuit_types"
+  add_foreign_key "designs", "tops_types"
+  add_foreign_key "designs", "users"
 end
