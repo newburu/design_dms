30e10c5fafdd69601814ca52696b76014421022c
Newburu <newburu@gmail.com>
2018-04-01 17:38:23 +0900

    Seedデータ追加

--- db/fixtures/sex.rb
+++ db/fixtures/sex.rb
@@ -1,4 +1,9 @@
 names = [
+  '大人(男)',
+  '大人(女)',
+  '子供(男)',
+  '子供(女)',
+  'フリー',
 ]
 names.each.with_index(1) do |name, idx|
   Sex.seed do |s|
--- db/fixtures/size.rb
+++ db/fixtures/size.rb
@@ -1,4 +1,13 @@
 names = [
+  'S',
+  'M',
+  'L',
+  'LL',
+  '3L',
+  '4L',
+  '5L',
+  '6L以上',
+  'フリー',
 ]
 names.each.with_index(1) do |name, idx|
   Size.seed do |s|
