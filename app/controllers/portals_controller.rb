51214b51b4196109fdfd324cee9b257d0da910cd
Newburu <newburu@gmail.com>
2018-04-02 22:10:18 +0900

    ポータルにランキング一覧を追加

--- app/controllers/portals_controller.rb
+++ app/controllers/portals_controller.rb
@@ -2,6 +2,7 @@ class PortalsController < ApplicationController
   
   def index
     @new_designs = Design.all.order(created_at: "DESC").limit(Settings.system[:portal][:limit])
+    @ranking_designs = Design.all.order(view_point: "DESC").limit(Settings.system[:portal][:limit])
   end
   
 end
