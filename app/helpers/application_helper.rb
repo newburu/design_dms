4874e99cb4a46b3e6f694e63d546e934c575d0a3
Newburu <newburu@gmail.com>
2018-04-11 19:30:40 +0900

    Twitterのトップページへのリンクを追加

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  
+  # Twitterのトップページへのリンク
+  def twitter_user_link(name, id)
+    link_to name, "#{Settings.system[:twitter][:url]}#{id}", target: "_blank"
+  end
+  
 end
